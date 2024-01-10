ccd261e6cf0a08e32f9244691cea6afff4c43114
Juan Camilo Cano Vargas <canojuancamilo99@gmail.com>
2024-01-10 18:47:25 -0500

    Se agrega el proyecto para manejar el frontend

---
+++ Frontend/FrontendPiedraPapelTijera/FrontendPiedraPapelTijera/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FrontendPiedraPapelTijera.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
