5c4f2b4e9989c0fd3702b9dcf14c61dc8a2b9726
Juan Camilo Cano Vargas <canojuancamilo99@gmail.com>
2024-01-10 17:02:46 -0500

    se ejecuta una nueva migracion

---
+++ Backend/BackendPiendraPapelTijeras/BackendPiendraPapelTijeras/Migrations/20240110220219_turnos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendPiendraPapelTijeras.Migrations
+{
+    /// <inheritdoc />
+    public partial class turnos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Resultado",
+                table: "Turnos");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaRegistro",
+                table: "Turnos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaRegistro",
+                table: "Turnos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resultado",
+                table: "Turnos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
