b38233ce7f2359a45193f0face0852cc21ede545
Juan Camilo Cano Vargas <canojuancamilo99@gmail.com>
2024-01-11 17:44:09 -0500

    se agrega mejora a la conexión de Backend

--- Frontend/FrontendPiedraPapelTijera/FrontendPiedraPapelTijera/Repositories/PiedraPapelTijeraRepository.cs
+++ Frontend/FrontendPiedraPapelTijera/FrontendPiedraPapelTijera/Repositories/PiedraPapelTijeraRepository.cs
@@ -2,6 +2,7 @@
 using FrontendPiedraPapelTijera.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Net.Http;
 using System.Reflection;
 
@@ -9,13 +10,16 @@ namespace FrontendPiedraPapelTijera.Repositories
 {
     public class PiedraPapelTijeraRepository : IPiedraPapelTijeraRepository
     {
-        private readonly string BaseUrl = "https://piendrapapeltijeras.azurewebsites.net/Api/PiedraPapelTijera";
+        private readonly IConfiguration _configuration;
         private readonly HttpClient _httpClient;
 
-        public PiedraPapelTijeraRepository(HttpClient httpClient)
+        public PiedraPapelTijeraRepository(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
+
+        private string BaseUrl => $"{_configuration.GetValue<string>("BackendApi:Url")}/Api/PiedraPapelTijera";
 
         public async Task<List<JugadorViewModel>> IniciarPartida(string PrimerJugador, string SegundoJugador)
         {
